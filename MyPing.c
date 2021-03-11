3803cd98357485b49081255ae156dca8248047c3
BuiDucTuong <toidihocbigdata@gmail.com>
2021-03-11 17:15:52 +0700

    fix bug: Destination unreachale

--- MyPing.c
+++ MyPing.c
@@ -3,17 +3,19 @@
 #include <string.h>
 #include <netinet/ip_icmp.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <sys/time.h>
 #include <unistd.h>
 
-#define BILLION 1000000000L
 #define DEFAULT_PORT 100
 #define DEFAULT_DATA_LENGTH 56
 #define IPV4_HEADER_LEN 20
+#define RECV_TIMEOUT 1 
 
-unsigned short addOneComplement16Bit(unsigned short x, unsigned short y){
+unsigned short addOneComplement16Bit(unsigned short x, unsigned short y)
+{
     unsigned int sum;
     sum = x + y;
     sum = (sum & 0xFFFF) + (sum >> 16);
@@ -32,10 +34,10 @@ unsigned short calculateCheckSum(void* headerAddress, int length)
     if (1 == length%2)
     {
         // process remain odd byte 
-        unsigned char* tempPointer = (unsigned char*) pointer;
-        unsigned short tempValue = 0x0;
-        tempValue += (*tempPointer) << 8;
-        checkSum = addOneComplement16Bit(checkSum, tempValue);
+        unsigned char* tempPointer  = (unsigned char*) pointer;
+        unsigned short tempValue    = 0x0;
+        tempValue         += (*tempPointer) << 8;
+        checkSum           = addOneComplement16Bit(checkSum, tempValue);
     }
     return ~checkSum;
 }
@@ -46,13 +48,12 @@ struct icmphdr* createICMPMessage(int sequenceNumber, char* dataAddress, int dat
     struct icmphdr* icmpMessage = (struct icmphdr*) dataAddress;
     memset(icmpMessage, 0x00, messageLength);
     // We are sending a ICMP_ECHO ICMP packet
-    icmpMessage->type = ICMP_ECHO;
-    icmpMessage->code = 0;
-    icmpMessage->checksum = 0;
-    icmpMessage->un.echo.sequence = htons(sequenceNumber);
-    // We don't need set the identifier becasue IPPROTO_ICMP socket will do it
-    // compute ICMP checksum here
-    icmpMessage->checksum = calculateCheckSum(icmpMessage, messageLength);
+    icmpMessage->type               = ICMP_ECHO;
+    icmpMessage->code               = 0;
+    icmpMessage->checksum           = 0;
+    icmpMessage->un.echo.sequence   = htons(sequenceNumber);
+    icmpMessage->un.echo.id         = getpid();
+    icmpMessage->checksum           = calculateCheckSum(icmpMessage, messageLength);
     return icmpMessage;
 }
 
@@ -70,6 +71,7 @@ int main(int argc, char **argv)
     sourceAddress.sin_family = AF_INET;
     destinationAddress.sin_family = AF_INET;
     destinationAddress.sin_port = htons(DEFAULT_PORT);
+    addressLength = sizeof(destinationAddress);
     // take arg[1] as Ipv4 address
     if (0 == inet_aton(argv[1], &destinationAddress.sin_addr))
     {
@@ -80,13 +82,15 @@ int main(int argc, char **argv)
     if ( 3 == argc)
     {
         numberOfPing = atoi(argv[2]);
-    }
-    else
-    {
-        numberOfPing = -1; 
+        if ( 1 > numberOfPing) 
+        {
+            printf("number of Ping must be a postive integer\n");
+            exit(1);
+        }
     }
     
     pingSocket = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
     if (-1 == pingSocket)
     {
         printf("Cannot create socket\n");
@@ -97,7 +101,7 @@ int main(int argc, char **argv)
         printf("Cannot getsockname\n");
         exit(1);
     }
-    waitTime.tv_sec = 1;
+    waitTime.tv_sec = RECV_TIMEOUT;
     waitTime.tv_usec = 0;
     if (-1 == setsockopt(pingSocket, SOL_SOCKET, SO_RCVTIMEO, (char*) &waitTime, sizeof(struct timeval)))
     {
@@ -106,71 +110,66 @@ int main(int argc, char **argv)
     }
 
     // Init some buffer and variable for send mesasge and revice
-    int dataLength = DEFAULT_DATA_LENGTH;
-    int messageLength = dataLength + sizeof(struct icmphdr);
-    unsigned int sequenceNumber = 0;
-    char* dataAddress = (char*) calloc(messageLength, sizeof(char));
-    struct icmphdr* imcpMessage = (struct icmphdr*) dataAddress; 
-    struct msghdr messageHeader;
-    char addressBuffer[128];
-    struct iovec iov;
-    unsigned char* repliedMessage = (unsigned char*)malloc(messageLength + IPV4_HEADER_LEN);
-    struct icmphdr *icmpRepliedMessage = (struct icmphdr *)(repliedMessage + IPV4_HEADER_LEN);
+    unsigned short sequenceNumber = 0;
+    int icmpDataLength = DEFAULT_DATA_LENGTH;
+    int icmpMessLength = icmpDataLength + sizeof(struct icmphdr);
 
-    iov.iov_base = (char *) repliedMessage;
-    iov.iov_len = messageLength;
+    unsigned char* sentBuffer = (unsigned char*) calloc(icmpMessLength, sizeof(char));
+    struct icmphdr* imcpHeader = (struct icmphdr*) sentBuffer; 
+    
+    unsigned char* recvBuffer = (unsigned char*)malloc(icmpMessLength + IPV4_HEADER_LEN);
+    struct icmphdr* icmpReplyHeader = (struct icmphdr*)(recvBuffer + IPV4_HEADER_LEN);
 
-    memset(&messageHeader, 0, sizeof(messageHeader));
-    messageHeader.msg_name = addressBuffer;
-    messageHeader.msg_namelen = sizeof(addressBuffer);
-    messageHeader.msg_iov = &iov;
-    messageHeader.msg_iovlen = 1;
+    struct iphdr* ipHeader = (struct iphdr*)recvBuffer;
 
     int i;
-    long pingIdx = 0;
+    int pingIdx = 0;
     for (;;)
     {
+        sleep(1);
         pingIdx++;
-        createICMPMessage(pingIdx, dataAddress, dataLength);
+        createICMPMessage(pingIdx, sentBuffer, icmpMessLength);
         gettimeofday(&begin, NULL);
         // Send ECHO Message
-        i = sendto(pingSocket, imcpMessage, (messageLength), 0, (struct sockaddr*)&destinationAddress, sizeof(destinationAddress));
-        printf("Sent ICMP_ECHO Message\n");
-        
-        // Wait to receive message
-        i = recvmsg(pingSocket, &messageHeader, 0);
-        gettimeofday(&end, NULL);
-
-        // Check results
+        i = sendto(pingSocket, sentBuffer, icmpMessLength, 0, (struct sockaddr*)&destinationAddress, sizeof(destinationAddress));
+        if ( 0 > i)
+        {
+            printf("Error in sendto\n\n");
+            continue;
+        }
+        // Receive ECHO REPLY message
+        inet_aton(argv[1], &destinationAddress.sin_addr);
+        i = recvfrom(pingSocket, recvBuffer, icmpMessLength + IPV4_HEADER_LEN, 0, (struct sockaddr*)&destinationAddress, &addressLength);
+        gettimeofday(&end, NULL);;
         if (0 > i)
         {
             printf("Error in recvfrom\n\n");
-            goto labelEnd;
+            continue;
         };
-        if (! 0 == calculateCheckSum(icmpRepliedMessage, dataLength))
+
+        if (! 0 == calculateCheckSum(icmpReplyHeader, icmpMessLength))
         {
             printf("Checksum status : BAD\n\n");
-            goto labelEnd;
+            continue;
         }
-        if (icmpRepliedMessage->type == ICMP_ECHOREPLY) 
+
+        if ((icmpReplyHeader->type == ICMP_ECHOREPLY) && 
+            (((unsigned int)ipHeader->saddr == (unsigned int)destinationAddress.sin_addr.s_addr)))
         {
             timeSpent =   (double)(end.tv_usec - begin.tv_usec) / 1000 
                         + (double)(end.tv_sec - begin.tv_sec) * 1000;  // uint in ms
-            printf("Reiveied ICMP_ECHOREPLY sequence number = %d\n", ntohs(icmpRepliedMessage->un.echo.sequence));
+            printf("Reiveied ICMP_ECHOREPLY sequence number = %d\n", ntohs(icmpReplyHeader->un.echo.sequence));
             printf("Round trip time = %lf ms \n\n", timeSpent);
         } 
         else 
-        {
-            printf("%ld\n", pingIdx);
-            printf("Not received message ICMP_ECHOREPLY\n\n");
-        }
+        {   
+            if (icmpReplyHeader->type ==ICMP_DEST_UNREACH)
+            {
+                printf("Destination Unreachable\n");
+            }
 
-labelEnd: sleep(1);
-        
-        if ( -1 == numberOfPing)
-        {
-            continue;
-        }
+        }        
+        if ( 0 == numberOfPing) continue;
         else
         {
             if (pingIdx == numberOfPing)
@@ -181,7 +180,7 @@ labelEnd: sleep(1);
     }
     
     // Release dynamic memory
-    free(repliedMessage);
-    free(imcpMessage);
+    free(sentBuffer);
+    free(recvBuffer);
     return 0;
 }
